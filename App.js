d3d3e73f17a6df7a56ed573fe8fc351fe14cacf9
Mari Luostarinen <mari.luostarinen@myy.haaga-helia.fi>
2024-03-22 19:11:33 +0200

    Iconeja lisätty, hakukenttä luotu

--- App.js
+++ App.js
@@ -4,52 +4,35 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Entypo, MaterialCommunityIcons } from '@expo/vector-icons';
+
 import Etusivu from './src/Etusivu';
 import Tiedot from './src/Tiedot';
 import Ruokaisa from './src/Ruokaisa';
 import Leivonta from './src/Leivonta';
 // navigaatio
 
-//const HomeStack = createStackNavigator();
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
-/*
-function HomeTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Koti" component={Etusivu} options={{ headerShown: false }} />
-      <Tab.Screen name="Ruokaisa" component={Ruokaisa} />
-      <Tab.Screen name="Leivonta" component={Leivonta} />
-    </Tab.Navigator>
-  );
-}
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Etusivu" component={HomeTabs} options={{ headerShown: false }} />
-        <Stack.Screen name="Tiedot" component={Tiedot} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-} */
 
 export default function StackNavigation() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="Koti" options={{ headerShown: false }}>
+          <Tab.Screen name="Koti" options={{ headerShown: false, tabBarIcon:() => ( <Entypo name="home" size={35}/> ) }}>
             {() => (
               <Stack.Navigator>
-               <Stack.Screen name="Etusivu" component={Etusivu} options={{ headerShown: false }} />
+               <Stack.Screen name="Etusivu"
+                component={Etusivu}
+                options={{ headerShown: false }}
+                />
                <Stack.Screen name="Tiedot" component={Tiedot} />
              </Stack.Navigator>     
             )}
           </Tab.Screen>
-          <Tab.Screen name="Ruokaisa" component={Ruokaisa} />
-          <Tab.Screen name="Leivonta" component={Leivonta} />
+          <Tab.Screen name="Ruokaisa" component={Ruokaisa} options={{ tabBarIcon:() => ( <MaterialCommunityIcons name="silverware-fork-knife" size={35} /> ) }} />
+          <Tab.Screen name="Leivonta" component={Leivonta} options={{ tabBarIcon:() => ( <MaterialCommunityIcons name="cupcake" size={38} /> ) }} />
         </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
--- src/Etusivu.js
+++ src/Etusivu.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+
 // etusivu, haku
 
 export default function Etusivu({ navigation }) {
+  const [text, setText] = useState('');
+  const buttonPressed = () => {
+    Alert.alert(text);
+  };
+
   return (
     <View style={styles.container}>
+      <View style={styles.searchBox}>
+        <FontAwesome name="search" size={24} color="black" style={styles.searchIcon} />
+        <TextInput style={{flex: 1}}
+          placeholder={'Hae resepti'}
+          onChangeText={text => setText(text)} value={text} />
+      </View>
       <Text>New project, here we come</Text>
-      <StatusBar style="auto" />
+      <Text>Kirjoita jotain</Text>
+      <Button onPress={buttonPressed} title='Press me' />
       <Button title="Tietoihin"
         onPress={() => navigation.navigate('Tiedot')}
       />
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -22,4 +37,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  searchBox: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderWidth: 0.5,
+    borderColor: '#000',
+    height: 40,
+    borderRadius: 5,
+    margin: 10,
+    width: '80%',
+  },
+  searchIcon: {
+    margin: 5,
+    height: 25,
+    width: 25,
+    alignItems: 'center',
+  }
 });
